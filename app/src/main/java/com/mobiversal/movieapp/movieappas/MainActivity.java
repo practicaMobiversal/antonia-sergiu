c37cd21a3248fcf6a62df10524ff30e35c732a24
lorinczantonia <lorincz_antonia@yahoo.com>
2019-07-10 16:10:26 +0300

    Second Activity.2

--- app/src/main/java/com/mobiversal/movieapp/movieappas/MainActivity.java
+++ app/src/main/java/com/mobiversal/movieapp/movieappas/MainActivity.java
@@ -1,10 +1,7 @@
 package com.mobiversal.movieapp.movieappas;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends ParentActivity {
@@ -13,7 +10,9 @@ public class MainActivity extends ParentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initClickListeners();
-     }
+        getSupportFragmentManager().beginTransaction().add(R.id.lyt_favorites, new FavoritesFragment(),"fragment");
+
+    }
 private void initClickListeners ()
 {
    findViewById(R.id.btn_favorites).setOnClickListener(new View.OnClickListener() {
