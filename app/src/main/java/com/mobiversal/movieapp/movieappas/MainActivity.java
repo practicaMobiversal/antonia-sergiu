51ad9dadc49d56051cca65932c2b792b7267781b
lorinczantonia <lorincz_antonia@yahoo.com>
2019-07-10 15:26:39 +0300

    Second Activity

--- app/src/main/java/com/mobiversal/movieapp/movieappas/MainActivity.java
+++ app/src/main/java/com/mobiversal/movieapp/movieappas/MainActivity.java
@@ -1,13 +1,31 @@
 package com.mobiversal.movieapp.movieappas;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends ParentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        initClickListeners();
+     }
+private void initClickListeners ()
+{
+   findViewById(R.id.btn_favorites).setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View view) {
+           openSavedMoviesActivity();
+
+       }
+   });
+}
+  private void openSavedMoviesActivity ()    {
+      Intent savedMoviesIntent =new Intent (this, SavedMoviesActivity.class);
+      startActivity(savedMoviesIntent);
+  }
 }
---
+++ app/src/main/java/com/mobiversal/movieapp/movieappas/ParentActivity.java
@@ -0,0 +1,55 @@
+package com.mobiversal.movieapp.movieappas;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ParentActivity extends AppCompatActivity {
+    protected String tag=MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(tag,"onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(tag, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(tag, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(tag,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(tag,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(tag,"onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(tag,"onRestart");
+    }
+}
+
+
+
+
